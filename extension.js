928fb60b1676c447202ba9ae14085304f2e2b6ac
Loïck <e0289033@csdessommets.qc.ca>
2023-02-16 12:30:39 -0500

    works whit //

--- extension.js
+++ extension.js
@@ -6,14 +6,50 @@ function activate(context) {
     let startLine = editor.selection.start.line;
     let endLine = editor.selection.end.line;
 
-    while (startLine > 0 && !editor.document.lineAt(startLine - 1).isEmptyOrWhitespace) {
+    // function isComment(text){
+    //   let iscomment;
+    //   if(text.search("//") == -1  || text.search("/**") == -1 || text.search("*") == -1 || text.search("*/") == -1)
+    //     iscomment = false;
+    //   else
+    //     iscomment = true;
+      
+    //   return iscomment;
+    // }
+
+    // Function to check if the given
+// string is a comment or not
+function isComment( line)
+{
+ 
+ 
+    // If two continuous slashes
+    // precedes the comment
+    let regex = new RegExp("//.*", );
+    let rex = regex.test(line);
+
+    let regexMul = new RegExp("/\\*.*?\\*/", );
+    let rexmul = regexMul.test(line);
+
+    let isCom;
+    if(rex){
+      isCom = true;
+    }else if (rexmul)
+    {
+      isCom = true;
+    }else
+      isCom = false;
+
+    return isCom;
+}
+
+    while (startLine > 0 && !editor.document.lineAt(startLine - 1).isEmptyOrWhitespace && isComment(editor.document.lineAt(startLine - 1).text)) {
       startLine -= 1;
     }
 
-    while (endLine < editor.document.lineCount - 1 && 
-      !editor.document.lineAt(endLine + 1).isEmptyOrWhitespace) {
+    while (endLine < editor.document.lineCount + 1 && !editor.document.lineAt(endLine + 1).isEmptyOrWhitespace && isComment(editor.document.lineAt(startLine + 1).text)) {
       endLine += 1;
     }
+
 
     // const startCharacter = editor.document.lineAt(startLine).firstNonWhitespaceCharacterIndex;
     const startCharacter = 0;
